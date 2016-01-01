89aa723632312e141cb47786b4091986803975e3
KawKaw88 <tjs24suburbia@gmail.com>
2016-01-01 09:46:04 -0600

    initial commit (initial project, then added one text string)

---
+++ app/src/androidTest/java/com/example/thomsmi/mod8_calit2bird_1/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.thomsmi.mod8_calit2bird_1;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
